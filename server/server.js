81e18e8fa7224d8d09b1cc8cf7977be8b42e4e06
Karrie Garzelloni <kgarzelloni@yahoo.com>
2022-02-22 23:46:20 -0700

    updated server.js and routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const routes = require( "./routes")
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -30,13 +31,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(routes);
 
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
 
 db.once("open", () => {
   app.listen(PORT, () => {
@@ -44,6 +40,5 @@ db.once("open", () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   })
 })
-};
-startApolloServer(typeDefs, resolvers);
+
 
